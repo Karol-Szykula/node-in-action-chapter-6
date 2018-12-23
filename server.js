c20967b031eb44f3db537b23b75b1eb23b0c090f
Karol-Szykula <karol.szykula@wp.pl>
2018-12-23 20:55:12 +0100

    Add logger middleware

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var connect = require('connect')
 
 var app = connect()
+
+app.use(logger)
+
 app.listen(3000)
+
+function logger(req, res, next) {
+    console.log('%s %s', req.method, req.url)
+    next()
+}

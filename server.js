bb89b6de2951bf03fb720c7e48a2701dbfc26869
Karol-Szykula <karol.szykula@wp.pl>
2018-12-23 20:57:00 +0100

    Add hello world method

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var connect = require('connect')
 var app = connect()
 
 app.use(logger)
+app.use(hello)
 
 app.listen(3000)
 
 function logger(req, res, next) {
     console.log('%s %s', req.method, req.url)
     next()
+}
+
+function hello(req, res) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello world')
 }

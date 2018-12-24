71ca505cd7a282375a6a85145c9c60606e23930f
Karol-Szykula <karol.szykula@wp.pl>
2018-12-24 14:43:32 +0100

    Clean file for next subchapter and add setup function

--- server.js
+++ server.js
@@ -1,50 +1,12 @@
-var connect = require('connect')
 
-var app = connect()
+function setup(format) {
+    var regexp = /:(\w+)/g
 
-app.use(logger)
-    .use('/admin', restrict)
-    .use('/admin', admin)
-    .use(hello)
-app.listen(3000)
-
-function logger(req, res, next) {
-    console.log('%s %s', req.method, req.url)
-    next()
-}
-
-function hello(req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello world')
-}
-
-function restrict(req, res, next) {
-
-    var authorization = req.headers.authorization
-    console.log(authorization)
-    if (!authorization) return next(new Error('User is not permitted'))
-
-    var parts = authorization.split('')
-    var scheme = parts[0]
-    var auth = new Buffer(parts[1], 'base64').toString().split(':')
-    var user = auth[0]
-    var pass = auth[0]
-
-    authenticateWithDatabase(user, pass, function (err) {
-        if (err) return next(err)
+    return function logger(req, res, next) {
+        var str = format.replace(regexp, function (match, property) {
+            return req[property]
+        })
+        console.log(str)
         next()
-    })
-
-}
-
-function admin(req, res, next) {
-    switch (req.url) {
-        case '/':
-            res.end('Try use /users')
-            break
-        case '/users':
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify((['tobi', 'Lukas', 'Jane'])))
-            break
     }
 }

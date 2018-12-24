b4c93ae0e1e773ab96aaa7922cbb5e77133b19e4
Karol-Szykula <karol.szykula@wp.pl>
2018-12-24 12:20:15 +0100

    Add restrict method

--- server.js
+++ server.js
@@ -16,3 +16,16 @@ function hello(req, res) {
     res.setHeader('Content-Type', 'text/plain')
     res.end('Hello world')
 }
+
+function restrict(req, res, next) {
+
+    var authorization = req.headers.authorization
+    if (!authorization) return next(new Error('User is not permitted'))
+
+    var parts = authorization.split('')
+    var scheme = parts[0]
+    var auth = new Buffer(parts[1], 'base64').toString().split(':')
+    var user = auth[0]
+    var pass = auth[0]
+
+}

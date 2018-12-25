e8da94f4da17fa57965c857f08c5938f749ade52
Karol-Szykula <karol.szykula@wp.pl>
2018-12-25 15:01:53 +0100

    Add basic configuration for error handling

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
-const connect = require('connet')
-const router = require('./middleware/router')
-const url = require('url')
+const connect = require('connect')
 
-const routes = {
-    GET: {
-        '/users': function (req, res) {
-            res.end('tobi', 'Lukas', 'Jane')
-        },
-        '/user/:id': function (req, res, id) {
-            res.end('user ' + id)
-        }
-    },
-    DELETE: {
-        '/user/:id': function (req, res, id) {
-            res.end('Removed user ' + id)
-        }
-    }
-}
+const api = connect()
+    .use(users)
+    .use(pets)
+    .use(errorHandler)
 
-connect()
-    .use(reqrite)
-    .use(showPost)
-    .use(router(routes))
+const app = connect()
+    .use(hello)
+    .use('/api', api)
+    .use(errorPage)
     .listen(3000)
-
-function setup(format) {
-    var regexp = /:(\w+)/g
-
-    return function logger(req, res, next) {
-        var str = format.replace(regexp, function (match, property) {
-            return req[property]
-        })
-        console.log(str)
-        next()
-    }
-}
